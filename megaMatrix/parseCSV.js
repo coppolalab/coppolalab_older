877fa9bb1959d5be6458b419b33f75eefde8a04e
Praphull Kumar <praphull27@gmail.com>
2014-10-30 20:55:52 -0700

    Updated parseCSV.js to take filename as argument.

--- megaMatrix/parseCSV.js
+++ megaMatrix/parseCSV.js
@@ -1,24 +1,29 @@
+var argv = require('optimist')
+							.usage('Parse a CSV file into JSON.\nUsage: $0')
+							.demand('f', 'o')
+							.alias('f', 'file')
+							.alias('o', 'output')
+							.default('o', 'output.json')
+							.describe('f', 'Input CSV file')
+							.describe('o', 'Output JSON file')
+							.argv;
+
 var Papa = require('babyparse')
 var fs = require("fs");
 
-fs.readFile('test1.csv', {encoding: 'UTF-8'}, function (err, data) {
+fs.readFile(argv.f, {encoding: 'UTF-8'}, function (err, data) {
 	if (err) throw err;
-	//console.log(data);
 
 	var results = Papa.parse(data, {delimiter: ",", header: true});
 	if ((results.errors).length == 0) {
-		//console.log(results);
-
-		var outputFilename = 'csvToJSON.json';
-		fs.writeFile(outputFilename, JSON.stringify(results, null, 4), function(err) {
+		fs.writeFile(argv.o, JSON.stringify(results, null, 4), function(err) {
 			if(err) {
 				console.log(err);
 			} else {
-				console.log("JSON saved to " + outputFilename);
+				console.log("JSON saved to " + argv.o);
 			}
 		}); 
 	} else {
 		console.log(results.errors);
 	}
 });
-
